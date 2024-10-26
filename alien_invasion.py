62957407c446bffe5ce3b7acf53d12b2b33729b8
YashdeepN <yashdeepneema@hotmail.com>
2024-10-26 07:51:14 -0500

    move the ship left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,9 +17,6 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
-
         self.ship = Ship(self)
 
     def run_game(self):
@@ -27,6 +24,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -34,6 +32,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
