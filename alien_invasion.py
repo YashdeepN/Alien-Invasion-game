a711e3f56be6ce8240b57babddb59548df0ffffd
YashdeepN <yashdeepneema@hotmail.com>
2024-10-26 07:22:46 -0500

    refactor update_screen method

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,19 +27,20 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-
-            # Redraw the screen during each pass through loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
