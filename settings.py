78158fbac8843ee8b244a3196dfc8c7329c970ab
YashdeepN <yashdeepneema@hotmail.com>
2024-10-27 22:30:40 -0500

    add bullet settings.

--- settings.py
+++ settings.py
@@ -9,5 +9,11 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Bullet Settings:
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
         # Ship setting
         self.ship_speed = 1.5
